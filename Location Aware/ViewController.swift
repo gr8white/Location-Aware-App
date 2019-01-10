e66d12be2c80bca4129a242416eb7fac615c4234
Derrick White <gr8white@uga.edu>
2019-01-10 08:51:39 -0600

    Initial Commit

---
+++ Location Aware/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location Aware
+//
+//  Created by Derrick White on 1/10/19.
+//  Copyright © 2019 Derrick White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
